a10dbfc54695eab7a06733224e11591332cd3aa0
Emily Chapman <emilychapman@Emilys-MacBook-Air.local>
2015-11-01 13:11:38 -0500

    Changing errors to use Exceptions

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -10,26 +10,12 @@ end
 
 post '/' do
 	@word = params[:word]
-	if valid_input?(@word)
+	begin
+		valid_input?(@word)
 		redirect "/anagrams/#{@word}"
-	elsif three_letters?(@word)
-		@error = "Enter a word with three distinct letters."
+	rescue Exception => error
+		@error = error.message
 		erb :index
-	elsif distinct_letters?(@word)
-		@error = "Exactly three letters, please!"
-		erb :index
-	else
-		@error = "There was an error. Try again!"
-		erb :index
-	end
-end
-
-
-def three_letters?(input)
-	if input.length == 3
-		true
-	else
-		false
 	end
 end
 
@@ -37,16 +23,20 @@ def distinct_letters?(input)
 	letter_array = input.chars
 	unique_letters = letter_array.uniq
 	if unique_letters.length < letter_array.length
-		false
-	else
-		true
+		raise Exception.new("Word must contain 3 unique characters.")
+	end
+end
+
+def three_letters?(input)
+	if input.length != 3
+		raise Exception.new("Word must be exactly 3 characters in length.")
 	end
 end
 
 def valid_input?(input)
 	if three_letters?(input) && distinct_letters?(input)
 		true
-	else
-		false
+	elsif three_letters?(input)
+	elsif distinct_letters?(input)
 	end
 end
