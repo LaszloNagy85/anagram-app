59bb6b181b649042f43c288fe0f7703e68c787fb
Emily Chapman <emilychapman@Emilys-MacBook-Air.local>
2015-11-02 20:46:07 -0500

    Removing brute force methods

--- app/models/word.rb
+++ app/models/word.rb
@@ -6,35 +6,4 @@ class Word < ActiveRecord::Base
   		alphabetized_characters = characters.sort
   		self.letters = alphabetized_characters.join
   	end
-	
-	def self.find_anagrams(string)
-	  letters = string.split(//)
-	  @solutions = []
-	  @anagrams = []
-
-	  letters.each do |letter|
-		remaining = letters.select { |l| l != letter }
-		@solutions << letter + remaining.join('')
-		@solutions << letter + reverse_letters(remaining).join('')
-	  end
-	  
-	  @solutions.each do |solution|
-	  	if Word.find_by_text(solution).present?
-	  		@anagrams << solution
-	  	end
-	  end
-
-	  @anagrams
-	end
-	
-	def self.reverse_letters(letters)
-		array_length = letters.length
-		reversed_letters = Array.new(array_length)
-
-		letters.each_with_index do |letter, index|
-			reversed_letters[array_length - index - 1] = letter
-		end
-	
-		return reversed_letters
-	end
 end
