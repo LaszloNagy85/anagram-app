f7661b36be9d4af99c138181e023921686b9574c
Emily Chapman <emilychapman@Emilys-MacBook-Air.local>
2015-11-06 22:28:40 -0500

    Fixing broken validation

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,39 +4,27 @@ end
 
 get '/anagrams/:word' do
 	@word = params[:word]
-	@anagrams = Word.find_anagrams(@word)
+	word_array = @word.chars.sort
+	@alphabetized_string = word_array.join
+	@anagrams = Word.where("letters=?", @alphabetized_string)
 	erb :show
+end
+
+def valid_input(input)
+	if input.match(/^[a-zA-Z]+[a-zA-Z]$/)
+		true
+	else
+		false
+	end
 end
 
 post '/' do
 	@word = params[:word]
-	begin
-		valid_input?(@word)
+	if valid_input(@word)
 		redirect "/anagrams/#{@word}"
-	rescue Exception => error
-		@error = error.message
 		erb :index
-	end
-end
-
-def distinct_letters?(input)
-	letter_array = input.chars
-	unique_letters = letter_array.uniq
-	if unique_letters.length < letter_array.length
-		raise Exception.new("Word must contain 3 unique characters.")
-	end
-end
-
-def three_letters?(input)
-	if input.length != 3
-		raise Exception.new("Word must be exactly 3 characters in length.")
-	end
-end
-
-def valid_input?(input)
-	if three_letters?(input) && distinct_letters?(input)
-		true
 	else
-		raise Exception.new("There was an error.")
+		@error = "Words must contain only Latin letters." 
+		erb :index
 	end
 end
